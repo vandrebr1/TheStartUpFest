fcec64a336ab334693806dd1ba03762deb08a406
Vandré Willian Priaro <vandrepriaro1@gmail.com>
2018-12-19 12:28:59 -0200

    Tela para votação

--- app/src/main/java/br/com/vandre/thestartupfest/atividades/StartupActivity.java
+++ app/src/main/java/br/com/vandre/thestartupfest/atividades/StartupActivity.java
@@ -1,7 +1,8 @@
-
 package br.com.vandre.thestartupfest.atividades;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
@@ -10,21 +11,31 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.UUID;
 
 import br.com.vandre.thestartupfest.R;
 import br.com.vandre.thestartupfest.app.Constantes;
 import br.com.vandre.thestartupfest.modelo.Startup;
+import br.com.vandre.thestartupfest.modelo.Voto;
 
 public class StartupActivity extends AppCompatActivity {
 
-    ImageView ivLogo;
-    TextView tvNome;
-    TextView tvSegmento;
-    TextView tvDescricao;
-    RatingBar rbProposta;
-    RatingBar rbApresentacao;
-    RatingBar rbDesenvolvimento;
+    private ImageView ivLogo;
+    private TextView tvNome;
+    private TextView tvSegmento;
+    private TextView tvDescricao;
+    private RatingBar rbProposta;
+    private RatingBar rbApresentacao;
+    private RatingBar rbDesenvolvimento;
+    private String id;
+    DatabaseReference databaseArtists;
 
 
     @Override
@@ -32,6 +43,29 @@ public class StartupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_startup);
         iniciar();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //attaching value event listener
+        databaseArtists.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //iterating through all the nodes
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    //getting artist
+                    Voto voto = postSnapshot.getValue(Voto.class);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void declarar() {
@@ -42,6 +76,10 @@ public class StartupActivity extends AppCompatActivity {
         rbProposta = findViewById(R.id.startup_rbProposta);
         rbApresentacao = findViewById(R.id.startup_rbApresentacao);
         rbDesenvolvimento = findViewById(R.id.startup_rbDesenvolvimento);
+
+        databaseArtists = FirebaseDatabase.getInstance().getReference("votos");
+
+        id = id(StartupActivity.this);
 
     }
 
@@ -94,6 +132,8 @@ public class StartupActivity extends AppCompatActivity {
             }
             ratingBar.setRating(stars);
 
+            enviarVoto();
+
             view.setPressed(false);
         }
         if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
@@ -103,6 +143,18 @@ public class StartupActivity extends AppCompatActivity {
         if (motionEvent.getAction() == MotionEvent.ACTION_CANCEL) {
             view.setPressed(false);
         }
+
+    }
+
+    private void enviarVoto() {
+        int votoProposta = (int) rbProposta.getRating();
+        int votoApresentacao = (int) rbApresentacao.getRating();
+        int votoDesenvolvimento = (int) rbDesenvolvimento.getRating();
+        String startupNome = tvNome.getText().toString();
+        Voto voto = new Voto(id, startupNome, votoProposta, votoApresentacao, votoDesenvolvimento);
+
+        databaseArtists.child(id).child(startupNome).setValue(voto);
+
     }
 
 
@@ -118,6 +170,24 @@ public class StartupActivity extends AppCompatActivity {
         finish();
         return null;
 
+    }
+
+    private static String uniqueID = null;
+    private static final String PREF_UNIQUE_ID = "PREF_UNIQUE_ID";
+
+    public synchronized static String id(Context context) {
+        if (uniqueID == null) {
+            SharedPreferences sharedPrefs = context.getSharedPreferences(
+                    PREF_UNIQUE_ID, Context.MODE_PRIVATE);
+            uniqueID = sharedPrefs.getString(PREF_UNIQUE_ID, null);
+            if (uniqueID == null) {
+                uniqueID = UUID.randomUUID().toString();
+                SharedPreferences.Editor editor = sharedPrefs.edit();
+                editor.putString(PREF_UNIQUE_ID, uniqueID);
+                editor.commit();
+            }
+        }
+        return uniqueID;
     }
 
 }
---
+++ app/src/main/java/br/com/vandre/thestartupfest/modelo/Voto.java
@@ -0,0 +1,37 @@
+package br.com.vandre.thestartupfest.modelo;
+
+public class Voto {
+    private String usuarioID;
+    private String startupNome;
+    private int votoProposta;
+    private int votoApresentacao;
+    private int votoDesenvolvimento;
+
+    public Voto(String usuarioID, String startupNome, int votoProposta, int votoApresentacao, int votoDesenvolvimento) {
+        this.usuarioID = usuarioID;
+        this.votoProposta = votoProposta;
+        this.votoApresentacao = votoApresentacao;
+        this.votoDesenvolvimento = votoDesenvolvimento;
+        this.startupNome = startupNome;
+    }
+
+    public String getUsuarioID() {
+        return usuarioID == null ? "" : usuarioID;
+    }
+
+    public int getVotoProposta() {
+        return votoProposta;
+    }
+
+    public int getVotoApresentacao() {
+        return votoApresentacao;
+    }
+
+    public int getVotoDesenvolvimento() {
+        return votoDesenvolvimento;
+    }
+
+    public String getStartupNome() {
+        return startupNome;
+    }
+}
